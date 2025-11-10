81cd9e79a852b4942a26f8ae9ed17b638c6ba482
Sadboipizza <rodrijos004@stu.sumnersd.org>
2025-11-10 13:42:59 -0800

    code and bigger

--- FpsTDef/Assets/sCRIPTS/PlayerContr.cs
+++ FpsTDef/Assets/sCRIPTS/PlayerContr.cs
@@ -1,16 +1,37 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class NewMonoBehaviourScript : MonoBehaviour
+public class PlayerContr : MonoBehaviour
 {
+    float inputX;
+    float inputY;
+    private Rigidbody rb;
+    public float speed = 10f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+       
+        rb = GetComponent<Rigidbody>();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 tempmove = rb.linearVelocity;
+        rb.linearVelocity = (tempmove.x * transform.forward) + (tempmove.y * transform.up) + (tempmove.z * transform.right);
+        tempmove.x = inputY * speed;
+        tempmove.z = inputX * speed;
+    }
+    public void Move(InputAction.CallbackContext context)
+    {
+        {
+            Vector2 InputAxis = context.ReadValue<Vector2>();
+            inputX = InputAxis.x;
+            inputY = InputAxis.y;
+        }
     }
 }
+
