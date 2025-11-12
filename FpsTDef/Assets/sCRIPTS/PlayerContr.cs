8b295951c821d5a45dacab38d685bbc70e54cbb2
Sadboipizza <rodrijos004@stu.sumnersd.org>
2025-11-12 12:19:32 -0800

    moving

--- FpsTDef/Assets/sCRIPTS/PlayerContr.cs
+++ FpsTDef/Assets/sCRIPTS/PlayerContr.cs
@@ -1,39 +1,61 @@
+
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+
 public class PlayerContr : MonoBehaviour
+
+
 {
+
+
+    private Rigidbody rb;
     float inputX;
     float inputY;
-    private Rigidbody rb;
-    public float speed = 10.0f;
+    public float speed = 50.0f;
+
     public PlayerInput input;
+
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-       input = GetComponent<PlayerInput>();
+        input = GetComponent<PlayerInput>();
+
+
+
         rb = GetComponent<Rigidbody>();
 
-        Cursor.lockState = CursorLockMode.Locked;
+
+
         Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 tempmove = rb.linearVelocity;
-        rb.linearVelocity = (tempmove.x * transform.forward) + (tempmove.y * transform.up) + (tempmove.z * transform.right);
-        tempmove.x = inputY * speed;
-        tempmove.z = inputX * speed;
+
+
+
+        //movement sysyem
+        Vector3 tempMove = rb.linearVelocity;
+        tempMove.x = inputY * speed;
+        tempMove.z = inputX * speed;
+
+        rb.linearVelocity = (tempMove.x * transform.forward) + (tempMove.y * transform.up) + (tempMove.z * transform.right);
+
+
     }
+
     public void Move(InputAction.CallbackContext context)
     {
-        {
-            Vector2 InputAxis = context.ReadValue<Vector2>();
-            inputX = InputAxis.x;
-            inputY = InputAxis.y;
-        }
+
+        Vector2 InputAxis = context.ReadValue<Vector2>();
+        inputX = InputAxis.x;
+        inputY = InputAxis.y;
+
     }
 }
-
