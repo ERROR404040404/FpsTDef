bba58c453ce909b0adadb2eb46386a6438bccbbf
William H. <87962047+ERROR404040404@users.noreply.github.com>
2025-11-20 12:16:29 -0800

    part2 of melee code

--- FpsTDef/Assets/sCRIPTS/Weapons/Melee.cs
+++ FpsTDef/Assets/sCRIPTS/Weapons/Melee.cs
@@ -7,6 +7,9 @@ public class Melee : MonoBehaviour
     public PlayerInput input;
     public Vector3 frw;
     public static bool queriesHitTriggers = true;
+    public float testVar;
+    public float ifDamage;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -16,14 +19,21 @@ public class Melee : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        ifDamage = 0;
+        var ray = new Ray(this.transform.position, this.transform.forward);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 5))
+        {
+             if (testVar == 1)
+             {
+                Debug.Log("Stabby!");
+                ifDamage = 1;
+             }
+        }
     }
 
     public void Attack(InputAction.CallbackContext context)
-    {
-        if (Physics.Raycast(transform.position, frw, 5))
-        {
-            print("stabby!");
-        }
+    {       
+        testVar = context.ReadValue<float>();
     }
 }
