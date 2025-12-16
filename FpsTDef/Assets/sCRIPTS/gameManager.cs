d7adbc4067f0941b48f3d4fbb1bd076053c9aac9
Sadboipizza <rodrijos004@stu.sumnersd.org>
2025-12-16 13:42:49 -0800

    UI?

--- FpsTDef/Assets/sCRIPTS/gameManager.cs
+++ FpsTDef/Assets/sCRIPTS/gameManager.cs
@@ -1,41 +1,77 @@
-using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
+using Unity.VisualScripting;
 
 public class gameManager : MonoBehaviour
 {
-   public follow_player enemy;
-    TextMeshProUGUI bits;
-    GameObject val;
-    TextMeshProUGUI wave;
-    GameObject waveNumb;
-
-
+    public bool isPaused = false;
+    GameObject pauseMenu;
+    PlayerContr player;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         if (SceneManager.GetActiveScene().buildIndex == 1)
         {
-
-            bits = GameObject.FindGameObjectWithTag("bits").GetComponent<TextMeshProUGUI>();
-           
+            player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerContr>();
+            pauseMenu = GameObject.FindGameObjectWithTag("PauseMenu");
+            pauseMenu.SetActive(false);
         }
     }
         // Update is called once per frame
         void Update()
         {
-        //bits.text = "bits:" + val.GetComponent<vaule>().value;
+    
         
 
     }
 
-    public void OnTriggerEnter(Collider other)
+    public void loadLevel(int level)
     {
-        if (other.CompareTag("bullet"))
+        Time.timeScale = 1;
+        SceneManager.LoadScene(level);
+    }
+    public void MainManu()
+    {
+        loadLevel(0);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+    public void Resume()
+    {
+        if (isPaused)
         {
-            enemy.health -= 50;
+            isPaused = false;
+
+            pauseMenu.SetActive(false);
+
+            Time.timeScale = 1;
+
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
         }
     }
+    public void Pause()
+    {
+        if (!isPaused)
+        {
+            isPaused = true;
+
+            pauseMenu.SetActive(true);
+
+            Time.timeScale = 0;
+
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+
+        else
+            Resume();
+    }
 }
+
 
